249201eee0971b28530838572ef2de3853c8a6cb
Rob Porter <rob@weeverapps.com>
2016-05-24 23:23:56 -0400

    Added more tests

--- src/Test.class.spec.js
+++ src/Test.class.spec.js
@@ -7,7 +7,7 @@ import should   from 'should';
 
 describe( 'the Test class', () => {
 
-    function mockController( req, res ) {
+    function mockControllerStatusSend( req, res ) {
         
         res.status( 200 ).send({
 
@@ -15,9 +15,38 @@ describe( 'the Test class', () => {
         });
     }
 
+    function mockControllerStatusError( req, res ) {
+
+        res.status( 500 );
+    }
+
+    function mockControllerQuery( req, res ) {
+
+        if ( req.query.test ) {
+
+            mockControllerStatusSend( req, res );
+        }
+        else {
+
+            mockControllerStatusError( req, res );
+        }
+    }
+
+    function mockControllerQueryBodyChain( req, res ) {
+
+        if ( req.query.queryTest && req.body.bodyTest ) {
+
+            mockControllerStatusSend( req, res );
+        }
+        else {
+
+            mockControllerStatusError( req, res );
+        }
+    }
+
     it( 'should handle response status if it is not matching', () => {
 
-        let myTest = new Test( mockController );
+        let myTest = new Test( mockControllerStatusSend );
 
         myTest.expect( 201 )
             .end( ( err, res ) => {
@@ -29,7 +58,7 @@ describe( 'the Test class', () => {
 
     it( 'should handle response status if it is matching', () => {
 
-        let myTest = new Test( mockController );
+        let myTest = new Test( mockControllerStatusSend );
 
         myTest.expect( 200 )
             .end( ( err, res ) => {
@@ -41,7 +70,7 @@ describe( 'the Test class', () => {
 
     it( 'should handle a JSON response sent to the client with the correct properties', () => {
 
-        let myTest = new Test( mockController );
+        let myTest = new Test( mockControllerStatusSend );
 
         myTest.expect( res => {
 
@@ -56,7 +85,7 @@ describe( 'the Test class', () => {
 
     it( 'should handle a JSON response sent to the client without the correct properties', () => {
 
-        let myTest = new Test( mockController );
+        let myTest = new Test( mockControllerStatusSend );
 
         myTest.expect( res => {
 
@@ -65,6 +94,59 @@ describe( 'the Test class', () => {
             .end( ( err, res ) => {
 
                 expect( !! err ).toBeTruthy();
+            })
+        ;
+    });
+
+    it( 'should pass query parameters along correctly, and handle chained expect calls', () => {
+
+        let myTest = new Test( mockControllerQuery );
+
+        myTest.query( { test: true } )
+            .expect( 200 )
+            .expect( res => {
+
+                res.body.should.have.property( 'success' );
+            })
+            .end( ( err, res ) => {
+
+                expect( !! err ).toBe( false );
+            })
+        ;
+    });
+
+    it( 'should pass query parameters along correctly, and handle chained expect calls with failures when appropriate', () => {
+
+        let myTest = new Test( mockControllerQuery );
+
+        myTest.query( { test: true } )
+            .expect( 400 )
+            .expect( res => {
+
+                res.body.should.have.property( 'success' );
+            })
+            .end( ( err, res ) => {
+
+                // we expect a thrown error here...
+                expect( !! err ).toBeTruthy();
+            })
+        ;
+    });
+
+    it( 'should pass query and body parameters chained together', () => {
+
+        let myTest = new Test( mockControllerQueryBodyChain );
+
+        myTest.query( { queryTest: true } )
+            .body( { bodyTest: true })
+            .expect( 200 )
+            .expect( res => {
+
+                res.body.should.have.property( 'success' );
+            })
+            .end( ( err, res ) => {
+
+                expect( !! err ).toBe( false );
             })
         ;
     });
